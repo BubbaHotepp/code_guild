7cacafb2e3d22ab569467719f2df054ab538221a
BubbaHotepp <stephen.simonsen@hotmail.com>
2020-02-26 21:27:12 -0800

    initial commit

---
+++ python/madlib.py
@@ -0,0 +1,6 @@
+noun_input1 = input('Please input a noun:')
+noun_input2 = input('Please input another noun:')
+adjective_input = input('Please input an adjective:')
+verb_input = input('Please input a verb:')
+
+print(f"Be careful not to vacuum under the {noun_input1} while you're cleaning the {noun_input2} or you may {verb_input} the {noun_input1} and make it {adjective_input}.")
