e788574e78b635bc7d4f71efb125741d65f9a1a9
BubbaHotepp <stephen.simonsen@hotmail.com>
2020-02-27 17:58:55 -0800

    fixing commit

---
+++ python/madlibversion2.py
@@ -0,0 +1,19 @@
+noun_list = []
+adjective_list = []
+verbs_list = []
+
+import random
+
+noun_input = input('Please enter 3 nouns separated by a comma')
+
+noun_input.split(",")
+
+adjective_input = input('Please enter 3 adjectives separated by a comma')
+
+adjective_input.split(",")
+
+verb_input = input('Please enter 3 verbs separated by a comma')
+
+verb_input.split(",")
+
+print("If you give a "random.sample(noun)" a "random.sample(noun)", they are going to ask for a "random.sample(noun)".  When you give them the "random.sample(adjective)" "random.sample(noun)", they will want to "random.sample(verb)".  When they are finished, they will "random.sample(verb)".  Then they will "random.sample(verb)" and "random.sample(verb)" to the "random.sample(noun)".  If not they will "random.sample(verb)" the "random.sample(adjective)" "random.sample(noun)".")
