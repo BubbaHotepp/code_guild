a0e93e53aebadad06a59928448d39a1097b090b8
BubbaHotepp <stephen.simonsen@hotmail.com>
2020-02-27 19:18:45 -0800

    update

--- python/madlibversion2.py
+++ python/madlibversion2.py
@@ -1,19 +1,15 @@
-noun_list = []
-adjective_list = []
-verbs_list = []
-
 import random
 
-noun_input = input('Please enter 3 nouns separated by a comma')
+noun_input = input('Please enter 3 nouns separated by a comma: ')
 
-noun_input.split(",")
+noun_list = noun_input.split(",")
 
-adjective_input = input('Please enter 3 adjectives separated by a comma')
+adjective_input = input('Please enter 3 adjectives separated by a comma: ')
 
-adjective_input.split(",")
+adjective_list = adjective_input.split(", ")
 
-verb_input = input('Please enter 3 verbs separated by a comma')
+verb_input = input('Please enter 3 verbs separated by a comma: ')
 
-verb_input.split(",")
+verb_list = verb_input.split(", ")
 
-print("If you give a "random.sample(noun)" a "random.sample(noun)", they are going to ask for a "random.sample(noun)".  When you give them the "random.sample(adjective)" "random.sample(noun)", they will want to "random.sample(verb)".  When they are finished, they will "random.sample(verb)".  Then they will "random.sample(verb)" and "random.sample(verb)" to the "random.sample(noun)".  If not they will "random.sample(verb)" the "random.sample(adjective)" "random.sample(noun)".")
+print(f"If you give a {random.choice(noun_list)} a {random.choice(noun_list)}, they are going to ask for a {random.choice(noun_list)}.  When you give them the {random.choice(adjective_list)} {random.choice(noun_list)}, they will want to {random.choice(verb_input)}.  When they are finished, they will {random.choice(verb_list)}.  Then they will {random.choice(verb_list)} and {random.choice(verb_list)} to the {random.choice(noun_list)}.  If not they will {random.choice(verb_list)} the {random.choice(adjective_list)} {random.choice(noun_list)}.")
