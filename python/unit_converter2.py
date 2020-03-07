024137a63c2147ba55ac8ed4b4f41f6393836706
BubbaHotepp <stephen.simonsen@hotmail.com>
2020-03-06 21:17:59 -0800

    edit

--- python/unit_converter2.py
+++ python/unit_converter2.py
@@ -1,26 +1,3 @@
-
-# print(unit())
-
-# def distance():
-#     x = int(input('Please enter the distance: '))
-#     return x
-
-# print(distance())
-
-# def meters():
-#         if x == 'f':
-#             x = 0.3408
-#             return x
-        
-#         elif x == 'k':
-#             x = 1000
-#             return x
-        
-#         elif x == 'm':
-#             x = 1609.34
-#             return x
-
-# print(meters(unit()))
 def meters(x):
     
     if x == 'f':
