c9079c240cd6186be65136a344d47d21f4318795
BubbaHotepp <stephen.simonsen@hotmail.com>
2020-03-12 18:26:59 -0700

    edit

--- python/practice01_fundamentals.py
+++ python/practice01_fundamentals.py
@@ -1,7 +1,26 @@
-# def odd_even(x):
+# First version prints whether odd or even
+
+def odd_even(x):
     
-#     if (x % 2) == 0:
-#         print (str(x) + ' is an even number.')
-#     else:
-#         print(str(x) + ' is an odd number.')
+    if (x % 2) == 0:
+        print (str(x) + ' is an even number.')
+    else:
+        print(str(x) + ' is an odd number.')
+
+# Second version determines True False for even
+
+def is_even(x):
+    if (x % 2) == 0:
+        print('True')
+    else:
+        print('False')
+
+odd_even(6)
+
+odd_even(3)
+
+is_even(9)
+
+is_even(8)
+
 
