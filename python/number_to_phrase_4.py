dc753a12ad32c6d9049af16ceae7d97dc1276827
BubbaHotepp <stephen.simonsen@hotmail.com>
2020-03-16 19:20:50 -0700

    edit

--- python/number_to_phrase_4.py
+++ python/number_to_phrase_4.py
@@ -1,7 +1,6 @@
 import string
 
-ones = {
-        1:'one',
+ones = {1:'one',
         2:'two',
         3:'three',
         4:'four',
@@ -23,69 +22,60 @@ teens = {11:'eleven',
          19:'nineteen',
         }
 
-ten_position = {2:'twenty',
-                3:'thirty',
-                4:'fourty',
-                5:'fifty',
-               }
-
-tens = {10:'ten',
+tens = {2:'twenty',
+        3:'thirty',
+        4:'fourty',
+        5:'fifty',
+        10:'ten',
         20:'twenty',
         30:'thirty',
         40:'fourty',
         50:'fifty',
        }
 
+def hours(x):
+
+    num_hour1 = (x // 10) % 10
+    num_hour2 = x % 10
+
+    if num_hour1 == 0:
+        return ones[num_hour2]
+
+    elif num_hour1 == 1:
+        return teens[x] 
+    
+    else:
+        y = tens_position[num_hour1]
+        z = ones[num_hour2]
+        return y + ' ' + z
+
+def minutes(x):
+
+    num_min1 = (x // 10) % 10
+    num_min2 = x % 10
+    if num_min1 == 0 and num_min2 == 0:
+        return 'o\'clock'
+
+    elif num_min1 == 0:
+        return ones[num_min2]
+
+    elif num_min1 == 1:
+        return teens[num_min2]
+
+    else:
+        y = tens[num_min1]
+        z = ones[num_min2]
+        return y + ' ' + z
+
 def main():
-        
-        time_input = input('Please enter the time hours first then minutes separated by space: ')
-        
-        print(time_input)
-        
-        time_split = time_input.split(' ')
 
-        print(time_split)
-
-        a = time_split[0]
-        b = time_split[1]
-
-        a = int(a)
-        b = int(b)
-
-        print(a)
-        print(b)
-
-        num_hour1 = (a // 10) % 10
-        num_hour2 = a % 10
-        num_min1 = (b // 10) % 10
-        num_min2 = b % 10
-
-        print(num_hour1)
-        print(num_hour2)
-        print(num_min1)
-        print(num_min2)                
-                
-        if num_hour1 > 0:
-            if num_hour1 > 1:
-                hour1 = 'twenty'
-                hour2 = ones[num_hour2]
-                hours = (hour1,hour2)
-
-            elif num_hour1 == 1:
-                hours = teens[a]
-
-        else:
-            hours = tens[a]
-
-        # print(f'{hours[0]} {hours[1]}')
-                
-        if num_min2 > 0:
-            if num_min1 > 0:
-                x = ten_position[num_min1]
-                y = ones[num_min2]
-                minutes = (x,y)
-                
-            if num_min1 == 0:
-
+    print('Time digit to word conversion. \nPlease input the time to convert.')
+    hours_input = int(input('Hours (1-12): '))
+    print(hours_input)
+    minutes_input = int(input('Minutes (0 - 59): '))
+    print(minutes_input)
+    am_pm = input('AM or PM: ')
+    
+    print(f'The time is: {hours(hours_input)} {minutes(minutes_input)} {am_pm}.')
 
 main()
