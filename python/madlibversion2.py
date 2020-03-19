2c30016ed1974791e3284a1230c2649720065edc
BubbaHotepp <stephen.simonsen@hotmail.com>
2020-03-19 14:04:37 -0700

    edit

--- python/madlibversion2.py
+++ python/madlibversion2.py
@@ -1,15 +1,18 @@
 import random
 
-noun_input = input('Please enter 3 nouns separated by a comma: ')
+def malib_choice(x):
+    return random.choice(x)
 
-noun_list = noun_input.split(",")
+def main():
+    noun_input = input('Please enter 3 nouns separated by a comma: ')
+    noun_list = noun_input.split(",")
 
-adjective_input = input('Please enter 3 adjectives separated by a comma: ')
+    adjective_input = input('Please enter 3 adjectives separated by a comma: ')
+    adjective_list = adjective_input.split(", ")
 
-adjective_list = adjective_input.split(", ")
+    verb_input = input('Please enter 3 verbs separated by a comma: ')
+    verb_list = verb_input.split(", ")
 
-verb_input = input('Please enter 3 verbs separated by a comma: ')
+    print(f"If you give a {madlib_choice(noun_list)} a {madlib_choice(noun_list)}, they are going to ask for a {madlib_choice(noun_list)}.  When you give them the {madlib_choice(adjective_list)} {madlib_choice(noun_list)}, they will want to {madlib_choice(verb_input)}.  When they are finished, they will {madlib_choice(verb_list)}.  Then they will {madlib_choice(verb_list)} and {madlib_choice(verb_list)} to the {madlib_choice(noun_list)}.  If not they will {madlib_choice(verb_list)} the {madlib_choice(adjective_list)} {madlib_choice(noun_list)}.")
 
-verb_list = verb_input.split(", ")
-
-print(f"If you give a {random.choice(noun_list)} a {random.choice(noun_list)}, they are going to ask for a {random.choice(noun_list)}.  When you give them the {random.choice(adjective_list)} {random.choice(noun_list)}, they will want to {random.choice(verb_input)}.  When they are finished, they will {random.choice(verb_list)}.  Then they will {random.choice(verb_list)} and {random.choice(verb_list)} to the {random.choice(noun_list)}.  If not they will {random.choice(verb_list)} the {random.choice(adjective_list)} {random.choice(noun_list)}.")
+main()
--- python/madlibversion3.py
+++ python/madlibversion3.py
@@ -1,34 +1,41 @@
 import random
 
-while True:
-    user_input = input('Would you like to play?: ')
+def madlib_choice(x):
+    return random.choice(x)
 
-    if user_input == 'y':
-        noun_input = input('Please enter 3 sciency sounding nouns separated by commas: ')
-        noun_list = noun_input.split(',')
+def main():
+    while True:
+        user_input = input('Would you like to play?: ')
 
-        verb_input = input('Please enter 3 pretentious verbs separated by commas: ')
-        verb_list = verb_input.split(',')
+        if user_input in ('y','Y'):
+            noun_input = input('Please enter 3 sciency sounding nouns separated by commas: ')
+            noun_list = noun_input.split(',')
 
-        adjective_input = input('Please enter 3 snarky adjectives separated by commas: ')
-        adjective_list = adjective_input.split(',')
+            verb_input = input('Please enter 3 pretentious verbs separated by commas: ')
+            verb_list = verb_input.split(',')
 
-        while True:
-            user_input2 = input('Would you like to read the story? \n Enter y/n to continue: ')
+            adjective_input = input('Please enter 3 snarky adjectives separated by commas: ')
+            adjective_list = adjective_input.split(',')
 
-            if user_input2 == 'y':
-                print(f'There once was a Doctor of {random.choice(noun_list)}. \nWho spent his days dreaming of {random.choice(noun_list)}. \nBut when he {random.choice(verb_list)} it all fell apart, \nUntil his {random.choice(adjective_list)} {random.choice(noun_list)} {random.choice(verb_list)} \nand he couldn\'t think anymore.')
-                continue
-            elif user_input2 == 'n':
-                print('I don\'t blame you, I wouldn\'t want to hear that nonsense either.')
-                break
+            while True:
+                user_input2 = input('Would you like to read the story? \n Enter y/n to continue: ')
+
+                if user_input2 in ('y','Y'):
+                    print(f'There once was a Doctor of {madlib_choice(noun_list)}. \nWho spent his days dreaming of {madlib_choice(noun_list)}. \nBut when he {madlib_choice(verb_list)} it all fell apart, \nUntil his {madlib_choice(adjective_list)} {madlib_choice(noun_list)} {madlib_choice(verb_list)} \nand he couldn\'t think anymore.')
+                    continue
+                elif user_input2 in ('n','N'):
+                    print('I don\'t blame you, I wouldn\'t want to hear that nonsense either.')
+                    break
+                else:
+                    print('Please enter only \'y\' or \'n\' to continue: ')
             else:
-                print('Please enter only y/n to continue:')
+                print('Please enter only \'y\' or \'n\' to continue: ')
     
-    elif user_input == 'n':
-        print('Maybe next time.')
-        break
+        elif user_input in ('n','N'):
+            print('Maybe next time.')
+            break
 
-    else:
-        print('Please only enter y/n to continue:')
+        else:
+            print('Please only enter \'y\' or \'n\' to continue: ')
 
+main()
