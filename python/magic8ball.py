44652282adfbe9cfd2a5d4c948c203bc2c63d6d9
BubbaHotepp <stephen.simonsen@hotmail.com>
2020-03-19 15:15:35 -0700

    edit

--- python/magic8ball.py
+++ python/magic8ball.py
@@ -1,11 +1,12 @@
 import random
 
-print('Welcome diviner. \nBe prepared to be amazed and astounded by the Magic 8 Ball.')
-
-print(input('Ask your question of the mystical Magic 8 Ball!!'))
-    
-def rand_choice():
+def magic_answer():
     answers = ['It is certain.', 'You can count on it.', 'No doubt fool.', 'Pretty sure.', 'Well, it could be.', 'Absolutely.', 'Yes!', 'No!', 'I really shouldn\'t tell you that.', 'Ask me again later.', 'The future is uncertain right now.', 'I wouldn\'t count on it.', 'My sources say you\'re dreaming.', 'Answer is a little hazy, try again.', 'Not looking so good.', 'The spirits point to yes.', 'Of course, you can trust it is so.', 'Ummm, How\'s the weather?']
     return random.choice(answers)
 
-print(f'The Glorious Magic 8 Ball says: {rand_choice()} ' )
+def main():
+    print('Welcome diviner. \nBe prepared to be amazed and astounded by the Magic 8 Ball.')
+    print(input('Ask your question of the mystical Magic 8 Ball!!'))
+    print(f'The Glorious Magic 8 Ball says: {magic_answer()} ' )
+
+main()
--- python/magic8ball2.py
+++ python/magic8ball2.py
@@ -1,6 +1,6 @@
 import random
 
-def rand_choice():
+def magic_answer():
     answers = ['It is certain.', 'You can count on it.', 'No doubt fool.', 'Pretty sure.', 'Well, it could be.', 'Absolutely.', 'Yes!', 'No!', 'I really shouldn\'t tell you that.', 'Ask me again later.', 'The future is uncertain right now.', 'I wouldn\'t count on it.', 'My sources say you\'re dreaming.', 'Answer is a little hazy, try again.', 'Not looking so good.', 'The spirits point to yes.', 'Of course, you can trust it is so.', 'Ummm, How\'s the weather?']
     return random.choice(answers)
 
@@ -11,12 +11,14 @@ def main():
     while True:
         question = input('Ask your question of the mystical Magic 8 Ball!!')
         
-        print(f'The Glorious Magic 8 Ball says: {rand_choice()} ' )
+        print(f'The Glorious Magic 8 Ball says: {magic_answer()} ' )
         
-        user_continue = input('Would you like to ask the \nMagic 8 Ball another question? \nPress y for yes any other key for no.')
+        user_continue = input('\nWould you like to ask the \nMagic 8 Ball another question? \n\'Y\' or \'y\' to continue any other key to quit: ')
 
-        if user_continue != 'y':
-            print('See you next time')
+        if user_continue in ('y','Y'):
+            continue
+        else:
+            print('\nSee you next time')
             break
 
 main()
