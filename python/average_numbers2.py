1aeb061eec50b29f03b2679009cdee06aef93f4a
BubbaHotepp <stephen.simonsen@hotmail.com>
2020-03-19 20:24:47 -0700

    edit

--- python/average_numbers2.py
+++ python/average_numbers2.py
@@ -1,11 +1,7 @@
 def main():
     num_list = []
-    
     run_sum = []
-
     num_input = ''    
-    
-
     x = 0
 
     while num_input != 'done':
@@ -13,20 +9,15 @@ def main():
         num_input = input('Please enter a number or \'done\' if you are finished: ')
         
         if num_input != 'done':
-            
             num_list.append(int(num_input))
-            
             print(num_list)
-
     
     for i in num_list:
         x += i
         run_sum.append(x)
-        print(run_sum)
         y = run_sum[-1]
         z = len(run_sum)
         print ('Running sum is: ' + str(y))
         print('Average is: ' + str(y/z))
-
 
 main()
