c6f6ae248408752f6f46b901c6a4591752f71eae
BubbaHotepp <stephen.simonsen@hotmail.com>
2020-03-19 20:21:32 -0700

    edit

--- python/average_numbers.py
+++ python/average_numbers.py
@@ -1,18 +1,14 @@
 def main():
     num_list = [5, 0, 8, 3, 4, 1, 6]
-    
     run_sum = []
-
     x = 0
 
     for i in num_list:
         x += i
         run_sum.append(x)
-        print(run_sum)
         y = run_sum[-1]
         z = len(run_sum)
-        print ('Running sum is: ' + str(y))
-        print('Average is: ' + str(y/z))
-
+        print (f'Running sum is: {y}')
+        print(f'Average is: {y/z}')
 
 main()
