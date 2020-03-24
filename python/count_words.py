f012b72f0e31375c8fff6cd58d83040f0941a9b9
BubbaHotepp <stephen.simonsen@hotmail.com>
2020-03-24 16:32:36 -0700

    'edit'

--- python/count_words.py
+++ python/count_words.py
@@ -7,16 +7,14 @@ def main():
     exclude = set(string.punctuation)
     book_file = ''.join(chr for chr in book_file if chr not in exclude)
     book_file = book_file.lower()
-    book_file = book_file.split()
-    length = len(book_file)
-    count = 0
-    while count < length:
-        for i in book_file:
-            if i in dict1:
-                dict1[i] += 1
-            else:
-                dict1[i] = 1
-        count += 1        
+    word_list = book_file.split()
+    
+    for i in word_list:
+        if i not in dict1:
+            dict1[i] = 0
+        else:
+            dict1[i] += 1
+
     words = list(dict1.items()) # .items() returns a list of tuples
     words.sort(key=lambda tup: tup[1], reverse=True)  # sort largest to smallest, based on count
     for i in range(min(10, len(words))):  # print the top 10 words, or all of them, whichever is smaller
