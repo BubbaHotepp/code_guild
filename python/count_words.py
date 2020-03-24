508a96b5cdd5cfbbf8e480038b7ac7a3187786cd
BubbaHotepp <stephen.simonsen@hotmail.com>
2020-03-23 20:10:04 -0700

    upload

--- python/count_words.py
+++ python/count_words.py
@@ -12,10 +12,10 @@ def main():
     count = 0
     while count < length:
         for i in book_file:
-            if i != dict1:
-                dict1[i] = 1
-            else:
+            if i in dict1:
                 dict1[i] += 1
+            else:
+                dict1[i] = 1
         count += 1        
     words = list(dict1.items()) # .items() returns a list of tuples
     words.sort(key=lambda tup: tup[1], reverse=True)  # sort largest to smallest, based on count
