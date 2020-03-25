463619b085094cd94a442624592fc9b4dd9a14ec
BubbaHotepp <stephen.simonsen@hotmail.com>
2020-03-24 21:58:20 -0700

    edit

--- python/count_words3.py
+++ python/count_words3.py
@@ -2,6 +2,7 @@ import string
 
 def main():
     dict1 = {}
+    pairs1 = {}
     file_input = open('61654.txt', encoding='utf8') #pg61664.txt or 61654.txt
     book_file = file_input
     exclude = set(string.punctuation)
@@ -9,12 +10,15 @@ def main():
     book_file = book_file.lower()
     word_list = book_file.split()
 
+    word_choice = input('Please enter a word to search for: ')
+
     for i in range(len(word_list) - 1):
-        word_pair = str(word_list[i] + ' ' + word_list[i + 1])
-        if word_pair not in dict1:
-            dict1[word_pair] = 1
-        else:
-            dict1[word_pair] += 1
+        if str(i) == word_choice:
+            word_pair = str(word_list[i] + ' ' + word_list[i + 1])
+            if word_pair not in dict1:
+                dict1[word_pair] = 1
+            else:
+                dict1[word_pair] += 1
 
     pairs = list(dict1.items()) 
     pairs.sort(key=lambda tup: tup[1], reverse=True)
