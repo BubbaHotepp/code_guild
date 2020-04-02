2a4d965c2e7441050e191e33f82bf1ae9cd410a2
BubbaHotepp <stephen.simonsen@hotmail.com>
2020-04-01 18:23:32 -0700

    'upload'

--- python/rain_data.py
+++ python/rain_data.py
@@ -1,27 +1,29 @@
 import datetime
+# class rain_data:
 
-class rain_data:
+#     def __init__(self, location, date, daily_total):
+#         self.location = location
+#         self.date = date
+#         self.daily_total = daily_total
 
-    def __init__(self, location, date, daily_total):
-        self.location = location
-        self.date = date
-        self.daily_total = daily_total
-
-    
+#     def date():
+#         date = datetime.datetime.strptime(y, '%d-%b-%Y')
+#         print(date.year)   # 2016
+#         print(date.month)  # 3
+#         print(date.day)    # 25
+#         print(date)  # 2016-03-25 00:00:00
+#         print(date.strftime('%d-%b-%Y'))  # 25-Mar-2016
 def main():
 
+    raw_data = []
     data_list = []
 
     data_file =''.join(open('columbia_ips.txt').readlines()[11:])
-    data_list = data_file.split('\n')
+    raw_data = data_file.split('\n')
+    del raw_data[-1]
+    for i in raw_data:
+        data_list.append(i[0:22])
     rain_data = tuple(data_list)
     print(rain_data)
-    
-    date = datetime.datetime.strptime('25-MAR-2016', '%d-%b-%Y')
-    print(date.year)   # 2016
-    print(date.month)  # 3
-    print(date.day)    # 25
-    print(date)  # 2016-03-25 00:00:00
-    print(date.strftime('%d-%b-%Y'))  # 25-Mar-2016
 
 main()
