a7c11a0989aa89185b0b10511fd9b0db9a6a33a0
BubbaHotepp <stephen.simonsen@hotmail.com>
2020-04-01 19:08:27 -0700

    'edit

--- python/rain_data.py
+++ python/rain_data.py
@@ -1,18 +1,5 @@
 import datetime
-# class rain_data:
 
-#     def __init__(self, location, date, daily_total):
-#         self.location = location
-#         self.date = date
-#         self.daily_total = daily_total
-
-#     def date():
-#         date = datetime.datetime.strptime(y, '%d-%b-%Y')
-#         print(date.year)   # 2016
-#         print(date.month)  # 3
-#         print(date.day)    # 25
-#         print(date)  # 2016-03-25 00:00:00
-#         print(date.strftime('%d-%b-%Y'))  # 25-Mar-2016
 def main():
 
     data_list = []
