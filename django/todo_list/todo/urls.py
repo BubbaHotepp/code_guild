6d5d63357d4d03a5bc2ecbe9bd8489e31e441c2b
BubbaHotepp <stephen.simonsen@hotmail.com>
2020-04-28 21:06:09 -0700

    edit

--- django/todo_list/todo/urls.py
+++ django/todo_list/todo/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.task_list, name='task_list'),
     path('', views.task_input, name='task_input'),
+    path('task/<int:pk>/', views.task_details name='task_details'),
+    path('task/new/', views.task_new, name='task_new'),
+    path('task/<int:pk>/edit/', views.task_edit, name='task_edit'),
 ]
