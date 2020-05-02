c8cabcb31e9c07d554e90e7960918e0367151bba
BubbaHotepp <stephen.simonsen@hotmail.com>
2020-05-01 18:08:06 -0700

    edit

--- django/todo/todoapp/views.py
+++ django/todo/todoapp/views.py
@@ -1,8 +1,39 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Task
+from .forms import TaskForm
 
 def task_list(request):
-    tasks = Task.objects.filter(posted_date__lte=timezone.now()).order_by('posted_date')    
+    tasks = Task.objects.filter(posted_date__lte=timezone.now()).order_by('posted_date')
     return render(request, 'todoapp/task_list.html', {'tasks': tasks})
 
+def task_detail(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    return render(request, 'todoapp/task_detail.html', {'task':task})
+
+def task_new(request):
+    if request.method == "POST":
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.created_by = request.user
+            post.posted_date = timezone.now()
+            post.save()
+            return redirect('task_detail', pk=task.pk)
+    else:
+        form = TaskForm()
+    return render(request, 'todoapp/task_edit.html', {'form': form})
+
+def task_edit(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    if request.method == "POST":
+        form = TaskForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.created_by = request.user
+            post.posted_date = timezone.now()
+            post.save()
+            return redirect('task_detail', pk=task.pk)
+    else:
+        form = TaskForm(instance=post)
+    return render(request, 'todoapp/task_edit.html', {'form': form})
