dd4744940240f0007d3150c54b8df8c317200604
BubbaHotepp <stephen.simonsen@hotmail.com>
2020-05-18 18:28:11 -0700

    edit

--- django/library/library_app/models.py
+++ django/library/library_app/models.py
@@ -3,6 +3,7 @@ from django.conf import settings
 from django.utils import timezone
 from django.urls import reverse
 from django.contrib.auth import get_user_model
+from datetime import date
 import uuid
 
 class Category(models.Model):
@@ -16,11 +17,23 @@ class Book(models.Model):
     author = models.ForeignKey('Author', on_delete=models.SET_NULL, null=True)
     category = models.ManyToManyField(Category)
 
+    def display_category(self):
+        return ', '.join([category.name for category in self.category.all()[:3]])
+
+    display_category.short_description = 'Category'
+
     def __str__(self):
         return self.title
 
     def get_absolute_url(self):
         return reverse('book-details', args[str(self.id)])
+
+class Book_copies(models.Model):
+    library_id = models.UUIDField(default=uuid.uuid4, editable=False, primary_key=True)
+    book_title = models.ForeignKey('Book', on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return f'{self.library_id} ({self.book.title})'
 
 class Author(models.Model):
     first_name = models.CharField(max_length=50)
@@ -37,11 +50,11 @@ class Author(models.Model):
         return f'{self.last_name}, {self.first_name}'
 
 class Catalog_record(models.Model):
-    catalog_id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    catalog_id = models.UUIDField(default=uuid.uuid4, editable=False, primary_key=True)
     book = models.ForeignKey('Book', on_delete=models.SET_NULL, null=True)
-    checkout_date = models.DateField(null=True, default=timezone.now)
+    checkout_date = models.DateField(null=True, default=timezone.now, blank=True)
     due_date = models.DateField(null=True, blank=True)
-    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, null=True)
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, null=True, blank=True)
 
     LOAN_STATUS = (
                    ('a', 'Available'),
@@ -56,8 +69,15 @@ class Catalog_record(models.Model):
                               default='a',        
                              )
 
+    @property
+    def overdue(self):
+        if self.due_date > date.today():
+            return True
+        else:
+            return False
+
     class Meta:
         ordering = ['due_date']
 
     def __str__(self):
-        return f'{self.id} ({self.book.title})'
+        return f'{self.catalog_id} ({self.book.title})'
--- django/library/library_app/views.py
+++ django/library/library_app/views.py
@@ -3,8 +3,30 @@ from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 from .models import Category, Book, Author, Catalog_record
 
-def Home(request):
-    return render(request, 'library_app/home.html')
+def home_page(request):
+    book_count = Book.objects.all().count()
+    total_copies_count = Catalog_record.objects.all().count()
+    available_book_count = Catalog_record.objects.filter(status='a').count()
+    author_count = Author.objects.count()
 
-def Book_list(request):
+    context = {
+        'book_count' : book_count,
+        'total_copies_count' : total_copies_count,
+        'available_book_count' : available_book_count,
+        'author_count' : author_count,
+    }
+
+    return render(request, 'library_app/home.html', context=context)
+
+def book_list(request):
     return render(request, 'library_app/book_list.html')
+
+def checkout_book(request):
+    pass
+
+def checkin_book(request):
+    pass
+
+def catalog_search(request):
+    pass
+
