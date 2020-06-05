fd791183b8f6fdfa8924993249a0c4260df06c37
BubbaHotepp <stephen.simonsen@hotmail.com>
2020-06-04 19:43:11 -0700

    edit

--- javascript/todo/todo.js
+++ javascript/todo/todo.js
@@ -1,11 +1,11 @@
 let list = document.querySelector('#taskList');
-let todoItem = document.querySelector('.js-form');
+let todoItem = document.querySelector('.task-form');
 let btn = document.querySelector('#button-addon2');
 let btn2 = document.querySelector('#button-addon3');
 
 
 btn.addEventListener('click', function() {
-    let input = document.querySelector('.js-task-input');
+    let input = document.querySelector('.task-input');
     let crossedOut = false;
     let taskText = input.value;
 
@@ -16,21 +16,24 @@ btn.addEventListener('click', function() {
         let liNode = document.createElement('LI');
         let liText = document.createTextNode(taskText);
         liNode.appendChild(liText);
-        liNode.innerHTML = liNode.innerHTML + '<button class="btn2 btn-primary" action="delete" type="button" id="button-addon3">Delete</button>';
+        liNode.innerHTML = liNode.innerHTML + '<button class="btn2 btn-primary" onClick="delTask()" type="button" id="button-addon3">Delete</button>';
         document.getElementById('taskList').appendChild(liNode);
     };
-
-
 });
 
 list.addEventListener('click', function(event) {
     if (event.target.tagName === 'LI') {
+        console.log('L1 works')
         event.target.classList.toggle('crossedOut')
         }
     }, false);  
 
-list.addEventListener('', function(event){
-    if (event.target.tagName === 'LI') {
-        event.target.remove()
-        }
-    });
+function delTask() {
+    console.log('function works')
+    list.addEventListener('click', function(event) {
+        console.log('still working')
+        item = event.target.parentElement
+        item.remove()
+        
+    })}
+
