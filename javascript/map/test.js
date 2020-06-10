e8a6d83151691057087fdcd94132721c6cb47071
BubbaHotepp <stephen.simonsen@hotmail.com>
2020-06-09 18:43:55 -0700

    edit

--- javascript/map/test.js
+++ javascript/map/test.js
@@ -2,11 +2,12 @@
 let axios = require('axios');
 
 axios.get('https://randomuser.me/api/?results=10&format=json&dl').then(response => {
-    response.data.results.map((result)) => {
+    response.data.results.map((result) => {
+        let newArray = Array.
         console.log(result)
         // let dataLength = response.data.results.length;
         // for (let item = 0; item <= dataLength; item++) {
         //     let itemData = response.data.results[item]
         // }
-    };
+    });
 });
